6a269ad7d9de9c4aa10a575e11b356e3e2cc5798
Jean <jean.pourroy@hpe.com>
2023-12-01 15:33:54 +0100

    New printing format

--- step1-sequential/scaling.sh
+++ step1-sequential/scaling.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-for i in {100..1500..200}; do
-    echo "$i: $(./prog1 $i)"
-    echo "$i: $(./prog2 $i)"
+# Print header
+printf "%-18s\t%-20s\t%-20s\n" "Taille de la matrice" "Temps prog1_naive (s)" "Temps prog2_optimized (s)"
+
+# Array of matrix sizes
+sizes=$(seq 100 100 1500)
+
+# Loop through sizes and print formatted output
+for i in "${sizes[@]}"; do
+    result1=$(./prog1_naive $i)
+    result2=$(./prog2_optimized $i)
+    time1=$(echo $result1 | awk '{print $(NF)}')  # Assuming the second last word is the time
+    time2=$(echo $result2 | awk '{print $(NF)}')  # Assuming the second last word is the time
+    printf "%-18s\t%-20s\t%-20s\n" "${i}*${i}" "$time1" "$time2"
 done
