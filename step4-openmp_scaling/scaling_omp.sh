7cc46753fe74131e6dc23be87bff1e091883cc9d
Mélian Fruchart <melian.fruchart@hpe.com>
2023-12-06 18:57:51 +0100

    renaming + deleting

--- step4-openmp_scaling/scaling_omp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for t in {1..8}; do
-    export OMP_NUM_THREADS=$t
-    echo "threads: $OMP_NUM_THREADS"
-    for i in {400..1000..200}; do
-        echo -e "\t$i: $(./prog2 $i)"
-    done
-done
